e1a5153a14e3fbd949f56cecc3fa4368d0ebf606
Jidraph1 <jidraphkimachia1@gmail.com>
2023-12-28 16:13:59 +0300

    loadtest, stresstest, smoketest

--- first_test.js
+++ first_test.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 
 export const options = {
     vus: 10,
-    duration:'60s'
+    duration:'10s'
 }
 export default function(){
-    http.get('http://test.k6.io');
+    http.get('https://test.k6.io');
 }
